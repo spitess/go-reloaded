b309c99c4edc3d3762ada836645575d911920b62
spitess <taousamine94@gmail.com>
2024-12-30 17:44:36 +0100

    error with this key xx . ..xx

--- punctuations.go
+++ punctuations.go
@@ -1,28 +1,23 @@
 package piscine
 
-/// amine      amine
-
-func trim(str string) string {
-	res := ""
-	for i, val := range str {
-		if val == ' ' && i+1 < len(str) && str[i+1] == ' ' {
-			continue
-		}
-		res += string(val)
-	}
-	return res
-}
+import (
+	"strings"
+)
 
 func Punctuations(s string) string {
-	slice := []rune(trim(s))
+	slice := []rune(s)
 
-	punc := ".,!?:;"
+	check := ".,!?:;"
+
 	for i, elm := range slice {
-		for _, val := range punc {
-			if (i+1 < len(slice)) && (elm == ' ' && slice[i+1] == val) {
+		for _, punc := range check { // check if we have a punc
+			if (i+1 < len(slice)) && (elm == ' ' && slice[i+1] == punc) {
 				slice[i], slice[i+1] = slice[i+1], slice[i]
 			}
 		}
 	}
-	return string(slice)
+	// fmt.Println("slice -->" + string(slice))
+	lastRes := strings.Join(SplitWhiteSpaces(string(slice)), " ")
+	// fmt.Println("res-->" + lastRes)
+	return lastRes
 }
