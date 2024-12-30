a8f2369fbf41fdc8bf6a62cb294a2183c27731bd
spitess <taousamine94@gmail.com>
2024-12-30 18:00:17 +0100

    a

---
+++ test/punctuations.go
@@ -0,0 +1,23 @@
+package piscine
+
+import (
+	"strings"
+)
+
+func Punctuations(s string) string {
+	slice := []rune(s)
+
+	check := ".,!?:;"
+
+	for i, elm := range slice {
+		for _, punc := range check { // check if we have a punc
+			if (i+1 < len(slice)) && (elm == ' ' && slice[i+1] == punc) {
+				slice[i], slice[i+1] = slice[i+1], slice[i]
+			}
+		}
+	}
+	// fmt.Println("slice -->" + string(slice))
+	lastRes := strings.Join(SplitWhiteSpaces(string(slice)), " ")
+	// fmt.Println("res-->" + lastRes)
+	return lastRes
+}
