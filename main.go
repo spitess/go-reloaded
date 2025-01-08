c959e0a488c446907ec156c04baeaab1470657fc
tamine <taousamine94@gmail.com>
2025-01-08 17:36:56 +0100

    finish

--- main.go
+++ main.go
@@ -3,29 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
-
 	piscine "piscine/functions"
 )
 
 func main() {
 	Args := os.Args[1:]
-
-	if len(Args) != 2 { // check if we have 2 args
+	if len(Args) != 2 {
 		fmt.Println("Err: You Must To Add Two Arguments")
 		return
 	}
-	content, err := os.ReadFile(Args[0]) // read the file
-	if err != nil {                      // check if we have error
+	content, err := os.ReadFile(Args[0])
+	if err != nil {
 		fmt.Println(err.Error())
 	}
-	if string(content) == "" { // check if the file is empty
+	if string(content) == "" {
 		fmt.Print("the file is empty")
 	}
-
-	// output := piscine.Punctuations(string(content))
-	fllags := piscine.Flags(string(content))
+	fllags := piscine.Flg(string(content))
 	punc := piscine.Punctuations(fllags)
-
-	os.WriteFile("result.txt", []byte(punc), 0o664) // copy and write the content to another file
-	fmt.Println("punc---->", punc)                  // debug output
+	vowel := piscine.HundlVowel(punc)
+	marks := piscine.HundleMarks(vowel)
+	hex := piscine.HundlHexAndBin(marks)
+	os.WriteFile("result.txt", []byte(hex), 0o664)
+	fmt.Println("last---->", hex)
 }
